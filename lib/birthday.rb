2df18e529effd3f2a5548b74d7e74039ebaaacd0
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-12 19:53:10 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,8 +5,8 @@
 # }
 
 def happy_birthday(birthday_kids)
-  happy_birthday.each do |name, age|
-    puts "Happy Birthday #{name}! You are now #{age} years old!"
+  happy_birthday.each do |kids_name, age|
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
   end
 end
 
