66bf9c8c24604a85243b38a0c61c6f9bbc742e3d
Dunia Al-Baghdadi <dunia-al-baghdadi@hotmail.com>
2019-03-12 20:04:30 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,3 +15,5 @@ def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
     if age =< 12
       puts "Happy Birthday #{name}! You are now #{age} years old!
+    end
+end
